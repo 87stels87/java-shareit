ff7bfadea1cd42caca5318dfc66543bb8addca5e
Okatev <okatev87@yandex.ru>
2023-07-11 23:37:43 +0300

    13 спринт. попытка 4. Поправил код стайл

--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.exceptions.BadRequestException;
 import ru.practicum.shareit.exceptions.NotFoundException;
-import ru.practicum.shareit.exceptions.ValidationException;
 
 import java.util.Collection;
 import java.util.stream.Collectors;
