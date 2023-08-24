711f1047eabe25b42b38acb1d38639945e9a063b
Okatev <okatev87@yandex.ru>
2023-08-24 22:51:33 +0300

    16 спринт. убрал лишние строки в енаме BookingState

--- gateway/src/main/java/ru.practicum.shareit/booking/dto/BookingState.java
+++ gateway/src/main/java/ru.practicum.shareit/booking/dto/BookingState.java
@@ -3,17 +3,11 @@ package ru.practicum.shareit.booking.dto;
 import java.util.Optional;
 
 public enum BookingState {
-
     ALL,
-
     CURRENT,
-
     PAST,
-
     FUTURE,
-
     WAITING,
-
     REJECTED;
 
     public static Optional<BookingState> from(String stringState) {
